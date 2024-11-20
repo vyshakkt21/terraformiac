25b4578e456d89c04a73828f8e0ed788d3be2ba9
vyshakkt21 <149256967+vyshakkt21@users.noreply.github.com>
2024-11-20 20:09:14 +0530

    Update c7-05-web-linuxvm-resource.tf

--- c7-05-web-linuxvm-resource.tf
+++ c7-05-web-linuxvm-resource.tf
@@ -30,7 +30,7 @@ resource "azurerm_linux_virtual_machine" "web_linuxvm" {
   network_interface_ids = [ azurerm_network_interface.web_linuxvm_nic.id ]
   admin_ssh_key {
     username = var.web_linuxvm_admin_user
-    public_key = file("${path.module}/ssh-keys/terraform-azure.pub")
+    public_key = file("${path.module}/ssh/terraform-azure.pub")
   }
   os_disk {
     caching = "ReadWrite"
